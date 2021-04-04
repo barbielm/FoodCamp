f08c71a08b615e9f4cd0b3237253643d95aed0ae
barbielm <barbosagabriel0508@gmail.com>
2021-04-04 07:14:54 -0300

    botão de fechar pedido

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,16 @@
+let n_pratos=0;
+let n_bebidas=0;
+let n_sobremesas=0;
+
+function fechar_pedido(){
+    const fechar = document.querySelector(".inferior .fechar_pedido");
+    if(n_pratos>0 && n_bebidas>0 && n_sobremesas>0){
+        fechar.classList.add("verde");
+        fechar.innerHTML = "Fechar Pedido";
+    }    
+}
+
+
 function marcar_opcao(id, classe){
     frase = "." + classe + "s" + " " + "." + classe;
     const opcoes = document.querySelectorAll(frase);
@@ -12,6 +25,18 @@ function marcar_opcao(id, classe){
     opcao.classList.add("borda-verde");
     check = opcao.querySelector(".ion-icon");
     check.classList.remove("oculto");
+    if(classe==="prato"){
+        n_pratos++;
+    }
+    if(classe==="bebida"){
+        n_bebidas++;
+    }
+    if(classe==="sobremesa"){
+        n_sobremesas++;
+    }
+    fechar_pedido()   
 
 }
+
+
 
