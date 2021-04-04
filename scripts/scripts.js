97d1775e04884fb5dfde70a75f6fae0f5bb030ee
barbielm <barbosagabriel0508@gmail.com>
2021-04-04 11:16:13 -0300

    versão beta

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -71,8 +71,10 @@ function habilitar_pedido(){
         valor_total = valor_total.toFixed(2)
 
         habilitar.classList.add("verde")
-        let link = document.getElementById("texto-botao")
-        link.innerText = "Fechar pedido"
+        let desabilitado = document.getElementById("desabilitado")
+        let habilitado = document.getElementById("habilitado")
+        desabilitado.classList.add("oculto")
+        habilitado.classList.remove("oculto")
 
         message += "Olá, gostaria de fazer o pedido:\n" + "- Prato: " + nome_prato + "\n"
         message += "- Bebida: " + nome_bebida + "\n"
@@ -84,12 +86,34 @@ function habilitar_pedido(){
     }    
 }
 
+function confirmar(){
+    const confirma = document.querySelector(".confirmar-pedido")
+    const conteudo = document.querySelector(".conteudo")
+    confirma.classList.remove("oculto")
+    conteudo.classList.add("borrado")
 
+    document.getElementById("nome_prato").innerText = nome_prato
+    document.getElementById("nome_bebida").innerText = nome_bebida
+    document.getElementById("nome_sobremesa").innerText = nome_sobremesa
 
-function solicitacao(){
+    document.getElementById("valor_prato").innerText = valor_prato.toFixed(2).replace(".",",")
+    document.getElementById("valor_bebida").innerText = valor_bebida.toFixed(2).replace(".",",")
+    document.getElementById("valor_sobremesa").innerText = valor_sobremesa.toFixed(2).replace(".",",")
+
+}
+
+function solicitar(){
     nome = prompt("Digite o seu nome: ")
     endereco = prompt("Digite o seu endereço: ")
     message = message + encodeURIComponent("\n\n\n" + "Nome: " + nome + "\n" +"Endereço: " + endereco)
     location.replace(message)
+}
+
+function recarregar(){
+    const confirma = document.querySelector(".confirmar-pedido")
+    const conteudo = document.querySelector(".conteudo")
+    confirma.classList.add("oculto")
+    conteudo.classList.remove("borrado")
+    
 }
 
